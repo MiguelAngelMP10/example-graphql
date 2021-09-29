a7997e22b485b8ad6e094b09eda2332b778d74c3
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-09-29 14:00:31 -0500

    Queries y Resolvers

---
+++ index.js
@@ -0,0 +1,26 @@
+"use strict"
+
+import { graphql, buildSchema } from "graphql"
+
+// definiendo el esquema
+const schema = buildSchema(`
+  type Query {
+    hello: String
+    saludo: String
+  }
+`)
+
+// Configurar los resolvers
+const resolvers = {
+    hello: () => {
+        return 'Hola Mundo'
+    },
+    saludo: () => {
+        return 'Hola a todos'
+    }
+}
+
+// Ejecutar el query hello
+graphql(schema, '{ saludo }', resolvers).then((data) => {
+    console.log(data)
+})
