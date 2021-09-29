60b99f921f68c4b707423f2bf947175c8118982f
Miguel Angel Muñoz Pozos <mmunozpozos@gmail.com>
2021-09-29 15:15:57 -0500

    Tipos personalizados

--- index.js
+++ index.js
@@ -1,36 +1,33 @@
-"use strict";
+'use strict'
 
-import { graphql, buildSchema } from "graphql";
-import express from "express";
+import { buildSchema } from 'graphql'
+import express from 'express'
+import { graphqlHTTP } from 'express-graphql'
 
-const app = express();
-import { graphqlHTTP } from "express-graphql";
-const port = process.env.port || 3000;
+import { readFileSync } from 'fs'
+import { join } from 'path'
+
+import resolvers from './lib/resolvers.js'
+
+const app = express()
+const port = process.env.port || 3000
 
 // definiendo el esquema
-const schema = buildSchema(`
-  type Query {
-    "Retorna un saludo al mundo"
-    hello: String
-  }
-`);
+const schema = buildSchema(
+  readFileSync(join('./', 'lib', 'schema.graphql'), 'utf-8')
+)
 
 // Configurar los resolvers
-const resolvers = {
-  hello: () => {
-    return "Hola Mundo";
-  },
-};
 
 app.use(
-  "/api",
+  '/api',
   graphqlHTTP({
     schema: schema,
     rootValue: resolvers,
-    graphiql: true,
+    graphiql: true
   })
-);
+)
 
 app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}/api`);
-});
+  console.log(`Server is listening at http://localhost:${port}/api`)
+})
